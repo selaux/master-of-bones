aa6a9872a697336fe47ce35e297b7dbd9f61a7ae
Stefan Lau <github@stefanlau.com>
2015-06-15 15:44:02 +0200

    Move synthetic generation to script.

--- synthetic_generation_helper.py
+++ synthetic_generation_helper.py
@@ -1,1 +1,12 @@
-__author__ = 'stefan'
+import sys
+from PyQt4 import QtGui
+
+import helpers.display as dh
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    dh.synthetic_model_generation()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
