254103ca9b3e73000f268ba31e500b874e3f7910
Stefan Lau <github@stefanlau.com>
2015-09-27 10:32:20 +0200

    Add some more presentation stuff.

--- generate_presentation_images.py
+++ generate_presentation_images.py
@@ -1,1 +1,19 @@
-__author__ = 'stefan'
+from skimage.io import imread
+from skimage.morphology import binary_erosion
+import matplotlib.pyplot as plt
+import algorithms.triangulation as at
+import numpy as np
+
+bone_pixels = binary_erosion(imread('thesis/presentation/outline-clustered.png'))
+
+points = np.nonzero(bone_pixels)
+points = np.vstack(points).transpose()
+points[:, 0] = bone_pixels.shape[0] - points[:,0]
+
+triangulation = at.triangulation(bone_pixels, alpha_shape=False)
+alpha_shape = at.triangulation(bone_pixels, alpha_shape=True)
+
+plt.triplot(points[:, 0], points[:, 1], alpha_shape.simplices.copy(), color='white')
+plt.gca().set_axis_bgcolor('black')
+plt.gcf().set_size_inches(bone_pixels.shape[0] / 100, bone_pixels.shape[1] / 100)
+plt.savefig('thesis/presentation/outline-alpha.png', dpi=100)
